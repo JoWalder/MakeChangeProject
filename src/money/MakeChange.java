5f06b89f376787e94ff18493e139b55369b78141
Jodi Walder <JoWalder303@gmail.com>
2017-08-04 15:33:06 -0600

    save 1

--- src/money/MakeChange.java
+++ src/money/MakeChange.java
@@ -1,5 +1,12 @@
 package money;
+import java.util.Scanner;
 
 public class MakeChange {
+	public static void main(String[] args) {
+		double[] cents = {0.25, 0.10, 0.05, 0.01};
+		int[] bills = {20, 10, 5, 1};
+		double[] prices = {};
+		
+	}
 
 }
