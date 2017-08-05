e2b15b6d7f50ed4f2451b922ecdc30f45b84c4d0
Jodi Walder <JoWalder303@gmail.com>
2017-08-05 07:23:03 -0600

    save 3

--- src/money/MakeChange.java
+++ src/money/MakeChange.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 
 public class MakeChange {
 	public static void main(String[] args) {
-		double price=0.0, tendered=0.0;
+		double price = 0.0, tendered = 0.0;
+		double cash;
 
 		Scanner kb = new Scanner(System.in);
+		
 		System.out.println("What is the price of the item you want to buy? ");
 		price = kb.nextDouble();
+
 		System.out.println("How much did the customer pay? ");
-		tendered =  kb.nextDouble();
-		
-		if (tendered < price ) {
+		tendered = kb.nextDouble();
+
+		if (tendered < price) {
 			System.out.println("Hey, you still owe me money!");
 		}
-		if(tendered == price) {
+
+		if (tendered == price) {
 			System.out.println("You paid the exact amount, no change back.");
 		}
 		
+		double overPay = tendered - price;
+		double dollar = (int)(overPay);
+		double cents = (overPay * 100) % 100;
+		System.out.println("I owe you " + dollar + " dollars and " + cents + " cents.");
 		
 		
 		
+	
 	}
 }
