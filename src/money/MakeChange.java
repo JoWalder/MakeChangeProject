59baf0c9b130b81816dc21df346ab91662de5046
Jodi Walder <JoWalder303@gmail.com>
2017-08-05 14:13:21 -0600

    save 6

--- src/money/MakeChange.java
+++ src/money/MakeChange.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MakeChange {
 	public static void main(String[] args) {
 		double price = 0.0, tendered = 0.0;
-		
+
 		Scanner kb = new Scanner(System.in);
 
 		System.out.println("What is the price of the item you want to buy? ");
@@ -16,17 +16,23 @@ public class MakeChange {
 
 		if (tendered < price) {
 			System.out.println("Hey, you still owe me money!");
+
 		}
 		kb.close();
-
 		if (tendered == price) {
 			System.out.println("You paid the exact amount, no change back.");
 		}
 
-		double overPay = tendered - price; // difference between
+		double overPay = (tendered - price); // difference between
 		System.out.println("I owe you: $ " + overPay);
 
-		int overPay2 = (int) (overPay) * 100;// casting to an int
+		int overPay2 = (int) (overPay * 100);// casting to an int
+		 //double overPay3 = overPay - overPay2;
+		int twenty = overPay2 / 2000;
+		if (twenty > 0) {
+			overPay2 = overPay2 % 2000;
+			System.out.println(twenty + "twenty dollar bill(s)");
+		}
 
 		int tens = overPay2 / 1000;
 		if (tens > 0) {
@@ -39,14 +45,14 @@ public class MakeChange {
 			overPay2 = overPay2 % 500;
 			System.out.println(fives + " five dollar bill(s)");
 		}
-		
-		int ones = overPay2/100;
+
+		int ones = overPay2 / 100;
 		if (ones > 0) {
-			overPay2 = overPay2 % 100;
-			System.out.println(ones + "one dollar bill(s)");
-			
-			
+			overPay = overPay % 100;
+			System.out.println(ones + " one dollar bill(s)");
+
 		}
+//System.out.println(overPay3);
 	}
 
 }
