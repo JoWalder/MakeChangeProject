9e800eec79377d8b51e8967bf47acaf3479cace1
Jodi Walder <JoWalder303@gmail.com>
2017-08-05 08:41:40 -0600

    save 4

--- src/money/MakeChange.java
+++ src/money/MakeChange.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class MakeChange {
 	public static void main(String[] args) {
-		double price = 0.0, tendered = 0.0;
-		double cash;
+		double price = 0.0, tendered = 0.0, cash;
 
 		Scanner kb = new Scanner(System.in);
-		
+
 		System.out.println("What is the price of the item you want to buy? ");
 		price = kb.nextDouble();
 
@@ -22,14 +21,43 @@ public class MakeChange {
 		if (tendered == price) {
 			System.out.println("You paid the exact amount, no change back.");
 		}
-		
+
 		double overPay = tendered - price;
-		double dollar = (int)(overPay);
-		double cents = (overPay * 100) % 100;
-		System.out.println("I owe you " + dollar + " dollars and " + cents + " cents.");
+		System.out.println("I owe you: $ " + overPay);
+
+		int overPay2 = (int) (overPay * 100);
+		//System.out.println(overPay2);
+
+		int dollars = overPay2 / 100;
+		System.out.println(dollars);
+		int twen = 0;
+		int ten = 0;
+		int five = 0;
+		int one = 0;
+				
+		if (dollars >= 20) {
+			 twen = dollars % 20;
+			 System.out.println("That's " + twen + " dollars");
+		}
 		
+		else if (dollars < 20 && dollars >= 10) {
+			ten = dollars % 10;
+			System.out.println("That's " + ten + " dollars");
+		}
 		
+		else if (dollars <= 5 && dollars > 1) {
+			five = dollars % 5;
+			System.out.println("That's " + five + " dollars");
+		}
 		
-	
+		else {
+			System.out.println("That's " + one + " dollars");
+		}
+
+		
+		int cents = overPay2 % 100;
+		
+		System.out.println(cents);
+
 	}
 }
